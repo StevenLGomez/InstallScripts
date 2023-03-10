dca9b00f83d43d0b716ab318f262033da596d9a6
admin <steve.gomez@biomerieux.com>
2023-03-10 16:17:31 -0600

    Modified setup script to work on Rocky86-podman

--- Kubernetes/set-config.sh
+++ Kubernetes/set-config.sh
@@ -1,10 +1,12 @@
 
 # For Red Hat OpenShift SANDBOX
-export USERNAME=bmxengineering
+export USERNAME=bmxengineering-dev
 export CLUSTER_NAME=api-sandbox-m3-1530-p1-openshiftapps-com:6443
 export CONTEXT=bmxengineering-dev/api-sandbox-m3-1530-p1-openshiftapps-com:6443/bmxengineering
 export API_SERVER_URL=https://api.sandbox.m3.1530.p1.openshiftapps.com:6443
-export TOKEN=sha256~aJVp5BAMCea5SnZwUTxMrmHy_GRvOfoQ4XXaO68pwJA
+export TOKEN=sha256~0ZDqlAgrKqJ9GRx8T9fQeXqXAbo2gg3k1Q_hpn3a2V8
+
+# oc login --token=sha256~0ZDqlAgrKqJ9GRx8T9fQeXqXAbo2gg3k1Q_hpn3a2V8 --server=https://api.sandbox-m3.1530.p1.openshiftapps.com:6443
 
 # The configuration steps
 kubectl config set-credentials ${USERNAME}/${CLUSTER_NAME} --token=${TOKEN}
