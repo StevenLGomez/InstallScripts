e12d62b707c1cdaaf732f508a1663fe170aed464
Steven Gomez <steve_gomez@usa.net>
2023-03-10 15:51:49 -0600

    Updated for new instance of OS Sandbox

--- Kubernetes/set-config.sh
+++ Kubernetes/set-config.sh
@@ -1,9 +1,10 @@
 
-# This group for Bmx OpenShift Sandbox
+# For Red Hat OpenShift SANDBOX
 export USERNAME=bmxengineering
 export CLUSTER_NAME=api-sandbox-m3-1530-p1-openshiftapps-com:6443
 export CONTEXT=bmxengineering-dev/api-sandbox-m3-1530-p1-openshiftapps-com:6443/bmxengineering
 export API_SERVER_URL=https://api.sandbox.m3.1530.p1.openshiftapps.com:6443
+export TOKEN=sha256~aJVp5BAMCea5SnZwUTxMrmHy_GRvOfoQ4XXaO68pwJA
 
 # The configuration steps
 kubectl config set-credentials ${USERNAME}/${CLUSTER_NAME} --token=${TOKEN}
